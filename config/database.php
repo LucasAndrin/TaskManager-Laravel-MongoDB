f6f48273d3a5ef25a699a6d87a90c088f571f903
Lucas Andrin <lucas.andrin.programmer@gmail.com>
2024-06-27 07:59:41 -0300

    fix: Session Driver

--- config/database.php
+++ config/database.php
@@ -39,21 +39,6 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        // 'pgsql' => [
-        //     'driver' => 'pgsql',
-        //     'url' => env('DB_URL'),
-        //     'host' => env('DB_HOST', '127.0.0.1'),
-        //     'port' => env('DB_PORT', '5432'),
-        //     'database' => env('DB_DATABASE', 'laravel'),
-        //     'username' => env('DB_USERNAME', 'root'),
-        //     'password' => env('DB_PASSWORD', ''),
-        //     'charset' => env('DB_CHARSET', 'utf8'),
-        //     'prefix' => '',
-        //     'prefix_indexes' => true,
-        //     'search_path' => 'public',
-        //     'sslmode' => 'prefer',
-        // ],
-
         'mongodb' => [
             'driver' => 'mongodb',
             'host' => env('DB_HOST'),
