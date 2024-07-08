5c5f2b4d403c85e761bdd5becf6ff6c1d567c7ab
Lucas Andrin <lucas.andrin.programmer@gmail.com>
2024-07-08 09:23:51 -0300

    refactor: Remove Repositories

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,19 +3,14 @@
 namespace App\Services;
 
 use App\Models\User;
-use App\Repositories\UserRepositoryInterface;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
 class AuthService
 {
-    public function __construct(
-        protected UserRepositoryInterface $users
-    ) { }
-
     public function register(string $name, string $email, string $password): ?User
     {
-        return $this->users->create([
+        return User::create([
             'name'=> $name,
             'email' => $email,
             'password' => $password
@@ -24,7 +19,7 @@ class AuthService
 
     public function login(string $email, string $password, string $device): string
     {
-        $user = $this->users->findByEmail($email);
+        $user = User::where('email', $email)->first();
 
         if (! ($user && Hash::check($password, $user->password))) {
             throw ValidationException::withMessages([
