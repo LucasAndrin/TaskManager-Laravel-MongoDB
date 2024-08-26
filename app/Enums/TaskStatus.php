084b37db6860a1d4caac53f4e7fc1b5b65a25e6a
Lucas Andrin <lucas.andrin.programmer@gmail.com>
2024-08-26 00:53:57 -0300

    feat: Enum TaskStatus Getters

--- app/Enums/TaskStatus.php
+++ app/Enums/TaskStatus.php
@@ -8,4 +8,24 @@ enum TaskStatus: int
     case Progress = 2;
     case Stopped = 4;
     case Completed = 8;
+
+    public function isPending(): bool
+    {
+        return $this === TaskStatus::Pending;
+    }
+
+    public function isProgress(): bool
+    {
+        return $this === TaskStatus::Progress;
+    }
+
+    public function isStopped(): bool
+    {
+        return $this === TaskStatus::Stopped;
+    }
+
+    public function isCompleted(): bool
+    {
+        return $this === TaskStatus::Completed;
+    }
 }
